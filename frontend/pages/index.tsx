ee765654c0780fa6f735ed54451fd61b18aaa4d7
kaikySantos <info.kaiky@gmail.com>
2022-07-04 13:00:59 -0300

    feat: frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next';
+import { FormEvent, useState } from 'react';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    console.log(data);
+  }
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.formContainer}>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
